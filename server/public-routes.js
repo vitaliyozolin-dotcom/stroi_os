8e21476cac224767b02378a46177af403ba2010b
vitaliyozolin-dotcom <vitaliyozolin@gmail.com>
2026-08-11 16:15:26 +0300

    Define explicit public VPS routes

---
+++ server/public-routes.js
@@ -0,0 +1,7 @@
+const PUBLIC_ROUTES = new Set([
+  '/api/health',
+  '/api/integrations/telegram/update',
+  '/api/public/leads',
+]);
+
+export const isPublicRoute = (url) => PUBLIC_ROUTES.has(url.pathname);
